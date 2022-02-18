9d1da8690221a32f356fc5b33b3f648504e2ff67
O0oMaximo0O <exspresspochta@gmail.com>
2022-02-18 17:06:30 +0300

    feat: writen unpack(), list(), structures List and File

--- list.c
+++ list.c
@@ -1,96 +1,154 @@
 #include "main.h"
 
-int list(char *archive_name)
+struct List * list(int archive_fd)
 {
-    int exit_code = 0;
-    char *resolved_file_path = calloc(sizeof(char), STR_MAX_SIZE + 1);
-    realpath(archive_name, resolved_file_path);
-
-    int file_d = open(resolved_file_path, O_RDONLY);
-    if (file_d < 0)
+    
+    lseek(archive_fd, 0, SEEK_SET);
+    
+    long read_c = 0;
+    int count;
+    void *buf = malloc(BUFFER_SIZE);
+    
+    if (buf == NULL)
     {
-        printf("error: %s\n", strerror(errno));
-        exit_code = 1;
-    } else
-    {
-        void *buffer = calloc(1, BUFFER_SIZE);
-        char *filename = calloc(sizeof(char), STR_MAX_SIZE + 1);
-        u_int64_t read_c;
-        u_int64_t buff_c;
-        char char_t = 0;
-        if (buffer == NULL || filename == NULL)
-        {
-            printf("error: Not enough memory. Requested %d bytes\n", BUFFER_SIZE + STR_MAX_SIZE + 1);
-            exit_code = 1;
-        } else
-        {
-            if (read(file_d, (void *) &char_t, sizeof(char)) != 1)
-            {
-                printf("error: %s\n", strerror(errno));
-                exit_code = 1;
-            } else {
-                if (char_t != RECORD_SEPARATOR)
-                {
-                    printf("error: Not archive\n");
-                    exit_code = 1;
-                } else {
-                    read_c = 0;
-                    // Rewind
-                    lseek(file_d, (long)read_c, SEEK_SET);
-
-                    read_c += read(file_d, (void *) &char_t, sizeof(char));
-                    while (char_t == RECORD_SEPARATOR)
-                    {
-                        memset(filename, 0, STR_MAX_SIZE + 1);
-                        u_int64_t size = 0;
-                        u_int64_t position = 0;
-                        size_t buff_pos = 0;
-                        // Read offset
-                        read_c += read(file_d, buffer, sizeof(u_int64_t));
-                        memcpy(&position, buffer, sizeof(u_int64_t));
-                        // Read size
-                        read_c += read(file_d, buffer, sizeof(u_int64_t));
-                        memcpy(&size, buffer, sizeof(u_int64_t));
-
-                        // Read filename
-                        buff_c = read(file_d, buffer, BUFFER_SIZE);
-                        read_c += buff_c;
-
-                        size_t str_len = strlen(filename);
-                        void *temp_a = memchr(buffer, RECORD_SEPARATOR, buff_c);
-                        void *temp_b = memchr(buffer, BINARY_SEPARATOR, buff_c);
-                        void *temp = (temp_a == NULL) ? temp_b : temp_a;
-                        while (temp == NULL)
-                        {
-                            memcpy(filename+str_len, buffer, buff_c);
-                            filename[str_len+buff_c] = 0;
-                            str_len = strlen(filename);
-                            buff_c = read(file_d, buffer, BUFFER_SIZE);
-                            read_c += buff_c;
-                            temp_a = memchr(buffer, RECORD_SEPARATOR, buff_c);
-                            temp_b = memchr(buffer, BINARY_SEPARATOR, buff_c);
-                            temp = (temp_a == NULL) ? temp_b : temp_a;
-                        }
-                        memcpy(filename+str_len, buffer, temp - buffer);
-                        filename[str_len+(temp - buffer)] = 0;
-
-                        // Print information
-                        printf("File: %s size: %lu bytes, offset: %lx\n", filename, size, position);
-
-                        // TODO
-                        // Set to next record
-                        read_c -= buff_c - (temp - buffer);
-                        lseek(file_d, (long)read_c, SEEK_SET);
-                        read_c += read(file_d, (void *) &char_t, sizeof(char));
-                    }
-                }
-            }
-        }
-        free(filename);
-        free(buffer);
-        close(file_d);
+        fprintf(stderr, "list: can't allocate buffer: %ud bytes\n", BUFFER_SIZE);
+        return NULL;
     }
-
-    free(resolved_file_path);
-    return exit_code;
+    
+    count = read(archive_fd, buf, sizeof(char));
+    if (count == -1)
+    {
+        perror("list");
+        free(buf);
+        return NULL;
+    }
+    
+    read_c += count;
+    
+    struct List *files_list = NULL;
+    
+    while (*buf == RECORD_SEPARATOR)
+    {
+        struct List *elem = insertElem(&files_list, -1, NULL, 0);
+        if (elem == NULL)
+        {
+            fprintf(stderr, "list: can't create new element in list of files\n");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        
+        elem->data = malloc(sizeof(struct File));
+        if (elem->data == NULL)
+        {
+            fprintf(stderr, "list: can't create new element in list of files\n");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        
+        ((struct File *)(elem->data))->name = (char *)malloc(STR_MAX_SIZE + 1);
+        if (((struct File *)(elem->data))->name == NULL)
+        {
+            fprintf(stderr, "list: can't create new element in list of files\n");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        ((struct File *)(elem->data))->name[0] = '\0';
+        
+        u_int64_t size = 0;
+        u_int64_t position = 0;
+        size_t buff_pos = 0;
+        
+        // Read offset
+        count = read(archive_fd, &(((struct File *)(elem->data))->size), sizeof(u_int64_t));
+        if (count == -1)
+        {
+            perror("list");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        if (count < sizeof(u_int64_t))
+        {
+            fprintf(stderr, "list: archive corrupted\n");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        read_c += count;
+        
+        // Read size
+        count = read(archive_fd, &(((struct File *)(elem->data))->size), sizeof(u_int64_t));
+        if (count == -1)
+        {
+            perror("list");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        if (count < sizeof(u_int64_t))
+        {
+            fprintf(stderr, "list: archive corrupted\n");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        read_c += count;
+        
+        // Read filename
+        count = read(archive_fd, buffer, BUFFER_SIZE);
+        if (count == -1)
+        {
+            perror("list");
+            free(buf);
+            removeList(&files_list, freeFile);
+            return NULL;
+        }
+        read_c += count;
+        
+        char *filename = ((struct File *)(elem->data))->name;
+        size_t str_len = strlen(filename);
+        
+        void *temp_a = memchr(buffer, RECORD_SEPARATOR, count);
+        void *temp_b = memchr(buffer, BINARY_SEPARATOR, count);
+        void *temp = (temp_a == NULL) ? temp_b : temp_a;
+        
+        while (temp == NULL)
+        {
+            if (str_len + count > STR_MAX_SIZE)
+            {
+                fprintf(stderr, "list: archive corrupted\n");
+                free(buf);
+                removeList(&files_list, freeFile);
+                return NULL;
+            }
+            memcpy(filename + str_len, buffer, count);
+            filename[str_len+count] = '\0';
+            str_len = strlen(filename);
+            count = read(archive_fd, buffer, BUFFER_SIZE);
+            if (count == -1)
+            {
+                perror("list");
+                free(buf);
+                removeList(&files_list, freeFile);
+                return NULL;
+            }
+            read_c += count;
+            temp_a = memchr(buf, RECORD_SEPARATOR, count);
+            temp_b = memchr(buf, BINARY_SEPARATOR, count);
+            temp = (temp_a == NULL) ? temp_b : temp_a;
+        }
+        memcpy(filename + str_len, buf, temp - buf);
+        filename[str_len + (temp - buf)] = '\0';
+        
+        // Set to next record
+        read_c -= count - (temp - buf);
+        lseek(archive_fd, read_c, SEEK_SET);
+        read_c += read(file_d, buf, sizeof(char));
+    }
+    
+    free(buf);
+    return files_list;
 }
--- unpack.c
+++ unpack.c
@@ -2,5 +2,162 @@
 
 int unpack(char *archive_name, char *dir_name)
 {
-    return 0;
+    int exit_code = 0;
+    
+    char *resolved_archive_path;
+    
+    resolved_archive_path = realpath(archive_name, NULL);
+    if (resolved_archive_path == NULL)
+    {
+        perror("unpack: archive");
+        exit_code = errno;
+        goto unpack_free_memory_step_0;
+    }
+    
+    char resolved_dir_path;
+    
+    resolved_dir_path = realpath(dir_name, NULL);
+    if (resolved_dir_path == NULL)
+    {
+        perror("unpack: directory");
+        exit_code = errno;
+        goto unpack_free_memory_step_1;
+    }
+    void *temp_ptr = realloc(resolved_dir_path, strlen(resolved_dir_path)+1 + 1);
+    if (temp_ptr == NULL)
+    {        
+        perror("unpack");
+        exit_code = errno;
+        goto unpack_free_memory_step_2;
+    }
+    resolved_dir_path = (char *)temp_ptr;
+    resolved_dir_path[strlen(resolved_dir_path)+2] = '\0';
+    resolved_dir_path[strlen(resolved_dir_path)+1] = '/';
+    
+    int archive_fd = open(resolved_archive_path, O_RDONLY);
+    
+    if (archive_fd == -1)
+    {
+        perror("unpack");
+        exit_code = errno;
+        goto unpack_free_memory_step_2;
+    }
+    
+    void *buf = malloc(BUFFER_SIZE);
+    
+    if (buf == NULL)
+    {
+        fprintf(stderr, "unpack: can't allocate buffer: %ud bytes\n", BUFFER_SIZE);
+        exit_code = errno;
+        goto unpack_free_memory_step_3;
+    }
+    
+    if (read(archive_fd, buf, 1) == -1)
+    {
+        perror("unpack");
+        exit_code = errno;
+        goto unpack_free_memory_step_4;
+    }
+    if (*buf != RECORD_SEPARATOR)
+    {
+        fprintf(stderr, "unpack: file is not archive\n");
+        exit_code = 1;
+        goto unpack_free_memory_step_4;
+    }
+    
+    struct List *files_list = list(archive_fd);
+    
+    if (files_list == NULL)
+    {
+        fprintf(stderr, "unpack: error for read of archive struct\n");
+        exit_code = 1;
+        goto unpack_free_memory_step_4;
+    }
+    
+    struct List *files_list_elem = files_list;
+    
+    while (files_list_elem != NULL)
+    {
+        struct File *file = files_list_elem->data;
+        char *full_filename = (char *)malloc(strlen(resolved_dir_path)+strlen(file->name)+1);
+        if (files_list == NULL)
+        {
+            perror("unpack");
+            exit_code = errno;
+            goto unpack_free_memory_step_5;
+        }
+        
+        strcpy(full_filename, resolved_dir_path);
+        strcpy(full_filename+strlen(resolved_dir_path), file->name);
+        
+        int file_fd = open(full_filename, O_CREAT | O_EXCL | O_WRONLY);
+        
+        if (file_fd == -1)
+        {
+            if (errno == EEXIST)
+            {
+                fprintf(stdout, "unpack: %s is exsisted and will not be unpacked\n", full_filename);
+                lseek(archive_fd, file->size + 1, SEEK_CUR);
+            }
+            else
+            {
+                free(full_filename);
+                perror("unpack");
+                exit_code = errno;
+                goto unpack_free_memory_step_5;
+            }
+        }
+        else
+        {
+            int count;
+            if (read(archive_fd, buf, 1) == -1)
+            {
+                close(file_fd);
+                free(full_filename);
+                perror("unpack");
+                exit_code = errno;
+                goto unpack_free_memory_step_5;
+            }
+            int read_c = 0;
+            do
+            {
+                count = read(archive_fd, buf, (read_c + BUFFER_SIZE <= file->size) ? BUFFER_SIZE : file->size - read_c);
+                if (count == -1)
+                {
+                    close(file_fd);
+                    free(full_filename);
+                    perror("unpack");
+                    exit_code = errno;
+                    goto unpack_free_memory_step_5;
+                }
+                count = write(file_fd, buf, count);
+                if (count == -1)
+                {
+                    close(file_fd);
+                    free(full_filename);
+                    perror("unpack");
+                    exit_code = errno;
+                    goto unpack_free_memory_step_5;
+                }
+                read_c += count;
+            }while(count != 0);
+            close(file_fd);
+        }
+        
+        free(full_filename);
+        files_list_elem = files_list_elem->next;
+    }
+    
+  unpack_free_memory_step_5:
+    removeList(&files_list, freeFile);
+  unpack_free_memory_step_4:
+    free(buf);
+  unpack_free_memory_step_3:
+    close(archive_fd);
+  unpack_free_memory_step_2:
+    free(resolved_dir_path);
+  unpack_free_memory_step_1:
+    free(resolved_archive_path);
+  unpack_free_memory_step_0:
+    return exit_code;
 }

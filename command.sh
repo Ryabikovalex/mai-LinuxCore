35eff7c1713d4f228177071080c3055e6cdb75fb
Ryabikovalex <ryabikov.alex@gmail.com>
2022-02-27 15:49:59 +0300

    fix: Fixed commands in shell

--- command.sh
+++ command.sh
@@ -7,16 +7,18 @@ function step_test() {
 }
 
 function step_build() {
-    step_test;
     cmake --build .build;
 }
 
 function step_run {
-    step_build;
     ./.build/osLinux;
 }
 
 case "$command" in
+  "ci")
+    cmake -S. -B.build;
+    step_test;
+    step_build;;
   "init")
     cmake -S. -B.build;;
   "test")
